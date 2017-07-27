e7ad39e904a388668a576eede9a71b25ea7c3788
Spencer Hilvitz <spencer.hilvitz@colorado.edu>
2017-07-27 10:23:27 -0600

    add seed data

--- db/seeds/dev/inventory.js
+++ db/seeds/dev/inventory.js
@@ -1,11 +1,29 @@
 
 exports.seed = (knex, Promise) => {
   return knex('inventory').del()
-    .then(function () {
-      return knex('inventory').insert([
-        {
-          title: ''
-          colName: 'rowValue1'},
+    .then(() => {
+      return Promise.all([
+        knex('inventory').insert(
+          {
+            title: 'lorem',
+            description: 'lorem ipsum',
+            image: 'https://www.americanologist.com/wp-content/uploads/2014/03/army-green-duffel-bag-owen-and-fred_grande.png',
+            price: '29.99'
+          }, 'id'),
+        knex('inventory').insert(
+          {
+            title: 'lorem2',
+            description: 'lorem ipsum 2',
+            image: 'https://www.americanologist.com/wp-content/uploads/2014/03/army-green-duffel-bag-owen-and-fred_grande.png',
+            price: '39.99'
+          }, 'id'),
+          knex('inventory').insert(
+            {
+              title: 'lorem3',
+              description: 'lorem ipsum 3',
+              image: 'https://www.americanologist.com/wp-content/uploads/2014/03/army-green-duffel-bag-owen-and-fred_grande.png',
+              price: '49.99'
+            }, 'id'),
       ]);
     });
 };
