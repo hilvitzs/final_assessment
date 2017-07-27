2f406fc68c7c76f97be721241037f883cc86a7a4
Spencer Hilvitz <spencer.hilvitz@colorado.edu>
2017-07-27 15:31:17 -0600

    get total working

--- app/index.js
+++ app/index.js
@@ -37,32 +37,48 @@ $('#inventory-section').on('click', '#addtocart-button', function() {
   const storeItem = JSON.stringify(newItemObject)
 
   localStorage.setItem(title, storeItem)
+
+  getCartItems();
 })
 
-$('.expand-button').on('click', () => {
+const getCartItems = () => {
+  let price = []
+  $('.cart-items').empty()
+
   if(localStorage.length === 0) {
-    $('h2').append(`
+    $('.cart-items').append(`
       <div>There are no items in your cart</div>
     `)
+  } else {
+    const localStorageKeys = Object.keys(localStorage)
+
+    const storageItems = localStorageKeys.map(key => {
+      return localStorage.getItem(key)
+    })
+
+
+    storageItems.map(storageItem => {
+      const parsedItem = JSON.parse(storageItem)
+
+      price.push(parseFloat(parsedItem.price.replace('$', '')))
+
+      return $('.cart-items').append(`
+        <p>${parsedItem.title}</p>
+        <p>${parsedItem.price}</p>
+        `)
+
+      })
+
+      let total = price.reduce((a, b) => a + b, 0);
+      $('#total').text(total)
+
   }
-  const localStorageKeys = Object.keys(localStorage)
-
-  const storageItems = localStorageKeys.map(key => {
-    return localStorage.getItem(key)
-  })
+}
 
 
-  storageItems.map(storageItem => {
-    const parsedItem = JSON.parse(storageItem)
-
-    return $('h2').after(`
-      <p>${parsedItem.title}</p>
-      <p>${parsedItem.price}</p>
-    `)
-  })
-
+$('.expand-button').on('click', () => {
   $('.cart-items').toggleClass('hidden')
 })
 
 
-window.onload = getInventoryItems();
+window.onload = getInventoryItems(), getCartItems();
