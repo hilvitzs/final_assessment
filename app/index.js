ef591f8ec3c06ef6995e12c500fe201cb170c30d
Spencer Hilvitz <spencer.hilvitz@colorado.edu>
2017-07-27 13:44:11 -0600

    add localstorage

--- app/index.js
+++ app/index.js
@@ -1,3 +1,61 @@
-$('button').on('click', () => {
-  console.log("something");
+const getInventoryItems = () => {
+  fetch('/api/v1/inventory', {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' }
+  })
+  .then(response => response.json())
+  .then(inventoryItems => prependInventoryItems(inventoryItems))
+  .catch(error => {error})
+}
+
+const prependInventoryItems = (array) => {
+  array.map(inventoryItem => {
+    return $('#inventory-section').prepend(`
+      <div id='inventory-item'>
+        <p id='title'>${inventoryItem.title}</p>
+        <p id='description'>${inventoryItem.description}</p>
+        <img id='item-image' src='${inventoryItem.image}'>
+        <p id='price'>$${inventoryItem.price}</p>
+        <button id='addtocart-button'>Add to Card</button>
+      </div>
+    `)
+  })
+}
+
+
+$('#inventory-section').on('click', '#addtocart-button', function() {
+  const title = $(this).siblings('#title').text();
+  const price = $(this).siblings('#price').text();
+
+  const itemObject = Object.assign({}, {
+    title,
+    price
+  })
+
+  const storeItem = JSON.stringify(itemObject)
+
+  localStorage.setItem(title, storeItem)
 })
+
+$('.expand-button').on('click', () => {
+  const localStorageKeys = Object.keys(localStorage)
+
+  const storageItems = localStorageKeys.map(key => {
+    return localStorage.getItem(key)
+  })
+
+
+  storageItems.map(storageItem => {
+    const parsedItem = JSON.parse(storageItem)
+    console.log(parsedItem);
+    return $('h2').after(`
+      <p>${parsedItem.title}</p>
+      <p>${parsedItem.price}</p>
+    `)
+  })
+
+  $('.cart-items').toggleClass('hidden')
+})
+
+
+window.onload = getInventoryItems();
--- server.js
+++ server.js
@@ -3,15 +3,37 @@ const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 const bodyParser = require('body-parser');
 const express = require('express');
+const path = require('path');
 const app = express();
 const config = require('dotenv').config();
 
 app.locals.title = 'AmazonBay';
 
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('app'));
 
 app.set('port', process.env.PORT || 3000);
+
+app.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname, '/app/index.html'));
+});
+
+app.get('/api/v1/inventory', (request, response) => {
+  database('inventory').select()
+  .then(inventoryItems => {
+    if(inventoryItems) {
+      response.status(200).json(inventoryItems)
+    } else {
+      return response.status(400).json({
+        error: 'There are no inventoryItems'
+      })
+    }
+  })
+  .catch(error => {
+    response.status(500).json({ error })
+  })
+})
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`)
