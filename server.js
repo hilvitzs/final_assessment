e534d445e1d683ff46f30c28c62918a6ef436c81
Spencer Hilvitz <spencer.hilvitz@colorado.edu>
2017-07-27 19:37:46 -0600

    change port

--- server.js
+++ server.js
@@ -67,8 +67,8 @@ app.post('/api/v1/order_history', (request, response) => {
   })
 })
 
-app.listen(app.get('port'), () => {
-  console.log(`${app.locals.title} is running on ${app.get('port')}.`)
+app.listen(port, () => {
+  console.log(`${app.locals.title} is running on ${port}}.`)
 })
 
 module.exports = app;
