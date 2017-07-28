f50e80d799954f7a6e6a15a3124481baae20d269
Spencer Hilvitz <spencer.hilvitz@colorado.edu>
2017-07-27 19:50:27 -0600

    try removing dirname

--- knexfile.js
+++ knexfile.js
@@ -30,7 +30,7 @@ module.exports = {
       directory: './db/migrations'
     },
     seeds: {
-      directory: __dirname + '/db/seeds/dev'
+      directory: './db/seeds/dev'
     },
     useNullAsDefault: true
   }
